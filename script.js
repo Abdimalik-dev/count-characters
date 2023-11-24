8e771c3f095990dca0a612ece07a5c3aed8de2f7
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-24 19:06:46 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Good Luck 💪🏾
+function count(num, string) 
+{
+    let count = 0
+    for (let m = 0; m < string.length; m++) 
+    {
+      if (string[m] == num) 
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+let leter = "c"
+let sentence = "fikrcamp coding Bootcamp"
+let result = count(leter, sentence)
+    console.log(result)
